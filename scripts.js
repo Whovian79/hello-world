a7e829daafa6a6eda0fcfe4b78f7126eaca6672c
Whovian79 <andrew.walsh001@student.swic.edu>
2020-01-29 20:16:42 -0600

    update in class work with homework attempt

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// PRIMITIVE data
+
+//We need a way to reuse or access data, so we create a REFERENCE.
+//'const' alerts JS to reserve some space in memory
+//We provide a name for this space in memory
+// We use the ASSIGNMENT OPERATOR '='
+//THIS TAKES THE VALUE on the right and assigns to the NAME REFERENCE.
+const myString = "strinadfasdfdsag";
+// Use 'let' ONLY if we plan to RE=ASSINGN  the VALUE REFERENCED by the VARIABLE
+let myNum = 23;
+
+// myStringIsThis = "something else";
+
+//homework 29 January
+const species = "Time Lord";
+let currentRegen = null;
+currentRegen = 4;
+
+//3 primitive data types
+//string
+("supercalafragalisticexpialadocious");
+//number
+let myPayRate = 3.14;
+//boolean
+//I understand this is true and false (if/then) I just can't think of a good example right now...
